56a800063de203400f378f2361e4a0439175a420
Ian Martinez <b.ianmartinez98@csu.fullerton.edu>
2023-10-01 15:25:22 -0700

    Project 1

---
+++ project1_starter.py
@@ -0,0 +1,20 @@
+#Ian Martinez
+# CPSC 335-09
+# b.ianmartinez98@csu.fullerton.edu
+
+
+
+
+  #
+  # Declare each variable that will used to distinguish each thing
+  # Set each value to zero since it'll be our initial vlaue for each vector
+  
+  # Input values are coming from text.file
+
+
+
+
+
+
+
+
